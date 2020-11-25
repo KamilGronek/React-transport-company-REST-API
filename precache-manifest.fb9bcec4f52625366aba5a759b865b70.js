aa24de6951b741d43a38bcf07e8c30f9c29c1301
Kamil_IT <kamillo.gronek@gmail.com>
2020-11-25 18:54:37 +0100

    Updates

---
+++ precache-manifest.fb9bcec4f52625366aba5a759b865b70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2341389d3fbab9820a0",
+    "url": "/React-transport-company-REST-API/static/css/main.df7abb59.chunk.css"
+  },
+  {
+    "revision": "c2341389d3fbab9820a0",
+    "url": "/React-transport-company-REST-API/static/js/main.a1129991.chunk.js"
+  },
+  {
+    "revision": "3708939c997c7f121c4d",
+    "url": "/React-transport-company-REST-API/static/js/runtime~main.7dece7a9.js"
+  },
+  {
+    "revision": "d7f9eba53255ee0e8aca",
+    "url": "/React-transport-company-REST-API/static/css/2.9bed7f5c.chunk.css"
+  },
+  {
+    "revision": "d7f9eba53255ee0e8aca",
+    "url": "/React-transport-company-REST-API/static/js/2.831ae969.chunk.js"
+  },
+  {
+    "revision": "b4c37aa159aa61ec18533ddbeecee260",
+    "url": "/React-transport-company-REST-API/index.html"
+  }
+];
